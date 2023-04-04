0ec38b4e31bdd51620d0a301413819107ad825d7
Usman-Malikk <usmannajeebmalik@gmail.com>
2023-04-04 21:14:30 +0500

    USman Mangotech account

--- Routes/Users/user.js
+++ Routes/Users/user.js
@@ -19,5 +19,10 @@ Router.get("/getalluser", GetAllUser, ErrorHandling);
 Router.get("/getuserbyid/:id", getUserById, ErrorHandling);
 Router.post("/updateuser", VerifyToken, updateUser, ErrorHandling);
 Router.delete("/:id", DeleteUser, ErrorHandling);
+Router.get('/welcome',(req,res)=>
+{
+  res.status(200).send('Welcome User')
+})
+
 
 export default Router;
